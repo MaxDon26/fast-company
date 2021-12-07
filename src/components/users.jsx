c3813d13cf3366b74e0acba61a1e0476de562378
Maxim Donskoy <maxwebdevelop@yandex.ru>
2021-12-07 22:46:53 +0300

    create test users tables

---
+++ src/components/users.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import api from "../api";
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll);
+  console.log(users);
+
+  const classes = (user) => {
+    const baseClass = "badge m-1 bg-";
+    return baseClass + user.color;
+  };
+
+  const renderPhrase = (number) => {
+    let people = users.length > 4 ? " человек" : " человека";
+    let color = users.length > 0 ? "bg-primary" : "bg-danger";
+    return (
+      <h1 className={"badge fs-4 " + color}>
+        {users.length > 0
+          ? number + people + " тусанет с тобой сегодня"
+          : "Никто с тобой не тусанет"}
+      </h1>
+    );
+  };
+
+  const handleDelete = (userId) => {
+    setUsers((prevState) =>
+      prevState.filter((user) => {
+        return user._id !== userId;
+      })
+    );
+  };
+  return (
+    <>
+      {renderPhrase(users.length)}
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>
+                {user.qualities.map((qual) => (
+                  <span className={classes(qual)} key={qual._id}>
+                    {qual.name}
+                  </span>
+                ))}
+              </td>
+
+              <td key={user.profession._id}>{user.profession.name}</td>
+              <td>{user.completedMeetings}</td>
+              <td>{user.rate}</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(user._id)}
+                >
+                  Удалить
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Users;
