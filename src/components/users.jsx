0d18ad308147c2a512361b01dfaba70fbdada082
Maxim Donskoy <maxwebdevelop@yandex.ru>
2022-01-27 21:30:19 +0300

    Refactoring code and added Bookmark

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,79 +1,31 @@
-import React, { useState } from "react";
-import api from "../api";
+import React from "react";
+import SearchStatus from "./searchStatus";
+import User from "./user";
 
-const Users = () => {
-  const [users, setUsers] = useState(api.users.fetchAll);
-  console.log(users);
-
-  const classes = (user) => {
-    const baseClass = "badge m-1 bg-";
-    return baseClass + user.color;
-  };
-
-  const renderPhrase = (number) => {
-    let people =
-      users.length > 4 || users.length === 1 ? " человек" : " человека";
-    let color = users.length > 0 ? "bg-primary" : "bg-danger";
-    return (
-      <h1 className={"badge fs-4 " + color}>
-        {users.length > 0
-          ? number + people + " тусанет с тобой сегодня"
-          : "Никто с тобой не тусанет"}
-      </h1>
-    );
-  };
-
-  const handleDelete = (userId) => {
-    setUsers((prevState) =>
-      prevState.filter((user) => {
-        return user._id !== userId;
-      })
-    );
-  };
+const Users = ({ users, ...rest }) => {
   return (
     <>
-      {renderPhrase(users.length)}
-      {users.length > 0 ? (
+      <SearchStatus length={users.length} />
+      {users.length === 0 ? (
+        false
+      ) : (
         <table className="table">
           <thead>
             <tr>
               <th scope="col">Имя</th>
               <th scope="col">Качества</th>
               <th scope="col">Профессия</th>
-              <th scope="col">Встретился раз</th>
+              <th scope="col">Встретился, раз</th>
               <th scope="col">Оценка</th>
               <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
             {users.map((user) => (
-              <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>
-                  {user.qualities.map((qual) => (
-                    <span className={classes(qual)} key={qual._id}>
-                      {qual.name}
-                    </span>
-                  ))}
-                </td>
-
-                <td key={user.profession._id}>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate}</td>
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(user._id)}
-                  >
-                    Удалить
-                  </button>
-                </td>
-              </tr>
+              <User key={user._id} {...user} {...rest} />
             ))}
           </tbody>
         </table>
-      ) : (
-        false
       )}
     </>
   );
