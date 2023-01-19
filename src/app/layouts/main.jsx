9877103cdb7c543c2a4d3578b3ee00f48f29aced
Maxim Donskoy <Maxwebdevelop@yandex.ru>
2023-01-19 16:24:55 +0300

    release version

---
+++ src/app/layouts/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="container mt-5">
+            <h3>
+                {" "}
+                <span className="text-primary fs-1">Fast Company</span> -
+                Приложение для быстрых встреч
+            </h3>
+        </div>
+    );
+};
+
+export default Main;
